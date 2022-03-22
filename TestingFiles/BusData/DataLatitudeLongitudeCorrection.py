a0b2481642ec4ef6c947682c464ee86a81da07c5
Nilay  Kabra <56975113+nKabra60@users.noreply.github.com>
2022-03-22 13:35:59 -0400

    Error filled code edit.

--- TestingFiles/BusData/DataLatitudeLongitudeCorrection.py
+++ TestingFiles/BusData/DataLatitudeLongitudeCorrection.py
@@ -11,10 +11,13 @@ print(file1Dataframe.dtypes)
 
 ddfile1 = file1Dataframe.drop_duplicates('Stop')
 
-file1Dataframe['ConstantLatitude'] = ''
-file1Dataframe['ConstantLongitude'] = ''
+print(ddfile1)
 
 print(file1Dataframe)
+file1Dataframe.loc[:, "ConstantLatitude"] = file1Dataframe.groupby('Stop').transform(
+    lambda x: ddfile1.query('Stop == @x').loc[0, "Latitude"])
 
+file1Dataframe.loc[:, "ConstantLongitude"] = file1Dataframe.groupby('Stop').transform(
+    lambda x: ddfile1.query('Stop == @x').loc[0, "Longitude"])
 
-print(ddfile1)
+print(file1Dataframe)
