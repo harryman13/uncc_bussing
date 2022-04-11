908df4ae05b8ce00cb93bf2049d61d74beca8894
Nilay  Kabra <56975113+nKabra60@users.noreply.github.com>
2022-04-10 23:39:43 -0400

    A Generalized Query Function for filtering data based on input

---
+++ Main Project/QueryFunctions.py
@@ -0,0 +1,19 @@
+import csv
+import pandas as pd
+
+file1 = 'file1Dataframe.csv'
+
+file1Dataframe = pd.read_csv(file1, low_memory=False)
+
+
+def queryItem(enterDataFrame, columnElement, columnTitle):
+    if columnTitle not in enterDataFrame.columns:
+        print("Error: The Column does not exist")
+        return 0
+    queryString = str(columnTitle) + ' == ' + str(columnElement)
+    enterDataFrame.query(queryString, inplace=True)
+    if enterDataFrame.empty:
+        print('No Results for Query')
+        return 0
+    return enterDataFrame
+
